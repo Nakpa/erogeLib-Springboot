a83a0c61a7c79bf84b7c70004078c368c0434b49
Nakpa <2424658571@qq.com>
2021-09-10 15:13:27 +0800

    favicon

--- src/main/java/com/library/eroge/erogelib/config/ErglibCorsConfigration.java
+++ src/main/java/com/library/eroge/erogelib/config/ErglibCorsConfigration.java
@@ -15,7 +15,7 @@ public class ErglibCorsConfigration {
         //1.添加CORS配置信息
         CorsConfiguration config = new CorsConfiguration();
         //1) 允许的域,不要写*，否则cookie就无法使用了
-        config.addAllowedOrigin("http://120.79.210.127");
+        config.addAllowedOrigin("*");
         //3) 允许的请求方式
         config.addAllowedMethod("OPTIONS");
         config.addAllowedMethod("POST");
